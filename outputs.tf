4cd6058cb830a30cae06d756b3a983d0a03ee8bb
Yonatan Koren <me@yonatankoren.com>
2021-06-24 10:50:24 -0400

    Add output for dns_name.

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "name" {
   value       = try(aws_globalaccelerator_accelerator.default[0].name, null)
 }
 
+output "dns_name" {
+  description = "DNS name of the Global Accelerator."
+  value       = try(aws_globalaccelerator_accelerator.default[0].dns_name, null)
+}
+
 output "listener_ids" {
   description = "Global Accelerator Listener IDs."
   value       = [for global_accelerator_listener in aws_globalaccelerator_listener.default : global_accelerator_listener.id]
