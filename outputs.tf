4218b46e2b5d73b1b28023b503552591642bd31b
Jonas Neves <72263638+jonasoneves@users.noreply.github.com>
2022-08-05 13:41:12 -0400

    Add output to expose the Global-Acc Route 53 Zone ID (#15)
    Co-authored-by: cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "dns_name" {
   value       = try(aws_globalaccelerator_accelerator.default[0].dns_name, null)
 }
 
+output "hosted_zone_id" {
+  description = "Route 53 zone ID that can be used to route an Alias Resource Record Set to the Global Accelerator."
+  value       = try(aws_globalaccelerator_accelerator.default[0].hosted_zone_id, null)
+}
+
 output "listener_ids" {
   description = "Global Accelerator Listener IDs."
   value       = [for global_accelerator_listener in aws_globalaccelerator_listener.default : global_accelerator_listener.id]
